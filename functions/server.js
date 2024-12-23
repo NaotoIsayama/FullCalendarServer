195d7558deb0b171db73a7e11457cca48638777d
Naoto <naotojacob@gmail.com>
2024-12-22 19:38:20 -0700

    import node fetch

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const fetch = require('node-fetch'); // Make sure fetch is imported
 
 const corsOptions = {
   origin: "https://NaotoIsayama.github.io",
